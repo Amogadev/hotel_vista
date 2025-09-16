2ef8cf5e64f1f1334164bfb9f6c300826e0117ef
Amoga devi <amogadevi.s10@gmail.com>
2025-09-16 04:44:58 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/hotel-vista/animated-shapes.tsx
+++ src/components/hotel-vista/animated-shapes.tsx
@@ -1,12 +1,14 @@
 
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AnimatedShapes = () => {
-  return (
-    <div className="absolute top-0 left-0 w-full h-full">
-      {Array.from({ length: 10 }).map((_, i) => (
+  const [shapes, setShapes] = useState<React.JSX.Element[]>([]);
+
+  useEffect(() => {
+    setShapes(
+      Array.from({ length: 10 }).map((_, i) => (
         <div
           key={i}
           className="animated-shape absolute bottom-[-100px] bg-white/20 rounded-full"
@@ -16,9 +18,11 @@ const AnimatedShapes = () => {
             animationDuration: `${15 + Math.random() * 10}s`,
           }}
         />
-      ))}
-    </div>
-  );
+      ))
+    );
+  }, []);
+
+  return <div className="absolute top-0 left-0 w-full h-full">{shapes}</div>;
 };
 
 export default AnimatedShapes;
