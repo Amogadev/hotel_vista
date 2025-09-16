9b78f4e75b79dfed24a8bc1cb619e569c182f3f2
Amoga devi <amogadevi.s10@gmail.com>
2025-09-16 05:40:59 +0000

    Try fixing this error: `Console Error: Encountered two children with the

--- src/components/hotel-vista/room-management-client.tsx
+++ src/components/hotel-vista/room-management-client.tsx
@@ -228,8 +228,8 @@ export default function RoomManagementDashboard() {
         ))}
       </div>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {rooms.map((room) => (
-          <RoomCard key={room.number} room={room} onSelectRoom={handleSelectRoom} />
+        {rooms.map((room, index) => (
+          <RoomCard key={`${room.number}-${index}`} room={room} onSelectRoom={handleSelectRoom} />
         ))}
       </div>
       {selectedRoom && (
