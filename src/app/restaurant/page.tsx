4b7efd1c76173d446d64cf6d9af3149a14e919b4
Amoga devi <amogadevi.s10@gmail.com>
2025-09-18 11:01:00 +0000

    in restaurant page i need top bar option like other pages

--- src/app/restaurant/page.tsx
+++ src/app/restaurant/page.tsx
@@ -1,10 +1,12 @@
 
+import Topbar from '@/components/hotel-vista/topbar';
 import RestaurantPOS from '@/components/hotel-vista/restaurant-pos';
 
 export default function RestaurantManagementPage() {
   return (
-    <div className="flex flex-col h-screen">
-      <main className="flex-1 overflow-hidden">
+    <div className="flex flex-col min-h-screen">
+      <Topbar />
+      <main className="flex-1 pt-14 h-[calc(100vh-3.5rem)]">
         <RestaurantPOS />
       </main>
     </div>
