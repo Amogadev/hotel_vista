819d388add5b2a626521226582e681c04d2db9b5
Amoga devi <amogadevi.s10@gmail.com>
2025-12-02 07:54:16 +0000

    key AIzaSyAl5XaghnB5BWx4naPXgM7r9ZqteTqgvos {returnSecureToken: true, em

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -26,7 +26,7 @@ const roleRedirects: { [key: string]: string } = {
 };
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('manager@example.com');
+  const [email, setEmail] = useState('admin@hotelvista.app');
   const [password, setPassword] = useState('admin');
   const [error, setError] = useState('');
   const [showPassword, setShowPassword] = useState(false);
